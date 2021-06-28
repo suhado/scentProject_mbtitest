378cbea2a1f5e7363523a82f7f4df6513f1fdf0e
suhado <suha@sookmyung.ac.kr>
2021-06-28 14:20:48 +0900

    meta태그 테스트 2pm

---
+++ share.js
@@ -0,0 +1,23 @@
+// Facebook 공유
+window.open(
+    'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent('https://boonboon.netlify.app/')
+  );
+  // Twitter 공유
+  window.open(
+    'https://twitter.com/intent/tweet?url=' + encodeURIComponent('https://boonboon.netlify.app/')
+  );
+
+// Kakao 공유
+Kakao.init('9969a7e1793ba969219b97e178898ae2');
+Kakao.Link.sendDefault({
+  objectType: 'feed',
+  content: { 
+    title: 'boonboon', 
+    imageUrl: 'https://boonboon.netlify.app/party_img/START.png', 
+    link: { webUrl: 'https://boonboon.netlify.app/' } 
+  },
+  buttons: [{ 
+    title: '링크 열기', 
+    link: { webUrl: 'https://boonboon.netlify.app/' } 
+  }]
+});
