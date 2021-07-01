0f2076c6dd16ce9ba311b5c259f0d82cee6cf42d
bogyeongPark <parkcsr0914@gmail.com>
2021-07-01 09:38:15 +0900

    함수 간단히

--- infoPack.js
+++ infoPack.js
@@ -6,69 +6,33 @@ var infoPack = {
     // recommend_item 3개 출력 (img,name,house)
     recommend: function() {
         var recommend_info = this.recommend_result[mbti];
-        var replace_recommend_img = [];
-        var replace_recommend_test = [];
-        
-        recommend_info.forEach(function (item) {
-            var push_img = replace_recommend_img.push(item["img"]);
-            var push_test = replace_recommend_test.push(
-            item["img"],
-            item["name"],
-            item["house"]
-            );
-            return [push_img, push_test];
-        });
-        
         var replace_recommend_info = "";
-        
-        var test = [];
         for (var i = 0; i < 3; i++) {
-            test = replace_recommend_test.slice(3 * i, 3 * i + 3);
-            replace_recommend_info += `<li class="pd_list"> <img class="contain" src=${test[0]} alt="대체이미지"> <div class="pre_product"> <span class="name">${test[1]}</span> <span class="house">${test[2]}</span> </div> </li>`;
+            replace_recommend_info += `<li class="pd_list"> <img class="contain" src=${recommend_info[i].img} alt="대체이미지"> <div class="pre_product"> <span class="name">${recommend_info[i].name}</span> <span class="house">${recommend_info[i].house}</span> </div> </li>`;
         }
         this.recommend_tag.innerHTML = replace_recommend_info;
     },
 
     moreInfo: function() {
-        var more_info = this.recommend_result[mbti];
-        var replace_more = [];
-        more_info.forEach(function (item) {
-            var push_more = replace_more.push(
-            item["img"],
-            item["name"],
-            item["house"],
-            item["type"],
-            item["top"],
-            item["middle"],
-            item["base"]
-            );
-            return push_more;
-        });
-        
+        var more_info = this.recommend_result[mbti];      
         var replace_more_info = "";
-        
-        const pd_length = parseInt(replace_more.length / 7);
-        var lastpd = [];
         
         function printNote(element, note) {
             var capNote = note.charAt(0).toUpperCase() + note.slice(1);
             return element.length ? `<div class="${note}">${capNote} Notes:<br> ${element}</div>` : ``;
         }
-        
-        for (var i = 0; i < pd_length; i++) {
-            lastpd = replace_more.slice(7 * i, 7 * i + 7);
-        
-            
+       
+        for (var i = 0; i < more_info.length; i++) {
             replace_more_info += `
                 <li class="pd_list"> 
-                    <img class="contain" src=${lastpd[0]} alt="대체이미지">
+                    <img class="contain" src=${more_info[i].img} alt="대체이미지">
                     <div class="product">
-                        <div class="name">${lastpd[1]}</div>
-                        <div class="house">${lastpd[2]}</div>
-                        <div class="type">Type: ${lastpd[3]}</div>
-                        ${printNote(lastpd[4], "top")}
-                        ${printNote(lastpd[5], "middle")}
-                        <div class="base">Base Notes:<br> ${lastpd[6]}</div>
+                        <div class="name">${more_info[i].name}</div>
+                        <div class="house">${more_info[i].house}</div>
+                        <div class="type">Type: ${more_info[i].type}</div>
+                        ${printNote(more_info[i].top, "top")}
+                        ${printNote(more_info[i].middle, "middle")}
+                        <div class="base">Base Notes:<br> ${more_info[i].base}</div>
                     </div>
                 </li>`;
         }
